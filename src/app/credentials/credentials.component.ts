66fa25b5a8426d436acd64d1d2a08fba8f33d890
Mauricio Arce Torrez <mauricio.arce.mat@gmail.com>
2022-11-14 22:04:25 -0300

    Added export credentials (#15)
    * Print bracelets (#14)
    
    * Add suggested changes
    
    * Add new dependences
    
    * Add Export bracelets option
    
    * update file name
    
    * add package lock
    
    * Improved some credential observations.
    
    Co-authored-by: Ruth Vargas <vargasmruth@gmail.com>

--- src/app/credentials/credentials.component.ts
+++ src/app/credentials/credentials.component.ts
@@ -1,6 +1,8 @@
 import { Component, ViewChildren, QueryList } from '@angular/core';
 
 import { jsPDF } from 'jspdf';
+import JSZip from 'jszip/dist/jszip';
+import * as FileSaver from 'file-saver';
 
 import { PostulantsService } from '../core/services/postulants.service';
 import { PostulantCredentialComponent } from '../shared/components/postulant-credential/postulant-credential.component';
@@ -52,6 +54,7 @@ export class CredentialsComponent {
 
   constructor(private postulantsService: PostulantsService) {}
 
+  // TODO: Remove this and the jsPDF library if it won't be used anymore
   printCredentials(): void {
     const pdf = new jsPDF('p', 'pt', 'legal');
     const quantityOfCredentials = this.credentials.length;
@@ -86,5 +89,45 @@ export class CredentialsComponent {
     });
 
     pdf.save('devfest-credentials.pdf');
+  }
+
+  exportImages(): void {
+    let itemsName = [];
+    let items = [];
+
+    this.credentials.forEach((credential) => {
+      let item = document.getElementById(
+        'assistant-' + credential.postulant.id,
+      );
+      const image = item.querySelector('canvas').toDataURL();
+
+      let blobData = this.convertBase64ToBlob(image);
+
+      const blob = new Blob([blobData], { type: 'image/png' });
+      itemsName.push('Assistant - ' + credential.postulant.fullName);
+      items.push(blob);
+    });
+
+    var zip = new JSZip();
+    items.forEach((value, i) =>
+      zip.file(itemsName[i] + '.jpg', value, { base64: true }),
+    );
+
+    zip
+      .generateAsync({ type: 'blob' })
+      .then((content) => FileSaver.saveAs(content, 'bracelets.zip'));
+  }
+
+  private convertBase64ToBlob(base64Image: string): Blob {
+    const parts = base64Image.split(';base64,');
+    const imageType = parts[0].split(':')[1];
+    const decodedData = window.atob(parts[1]);
+    const uInt8Array = new Uint8Array(decodedData.length);
+
+    for (let i = 0; i < decodedData.length; ++i) {
+      uInt8Array[i] = decodedData.charCodeAt(i);
+    }
+
+    return new Blob([uInt8Array], { type: imageType });
   }
 }
